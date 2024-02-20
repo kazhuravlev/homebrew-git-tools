7201215263bac39d56171c6166250e6a47bc1f90
goreleaserbot <kazhuravlev+goreleaserbot@fastmail.com>
2024-02-20 17:55:25 +0100

    Brew formula update for git-tools version v0.7.0

--- Formula/git-tools.rb
+++ Formula/git-tools.rb
@@ -5,13 +5,13 @@
 class GitTools < Formula
   desc "Git tools allow you to manage your git repository easily."
   homepage "https://github.com/kazhuravlev/git-tools"
-  version "0.6.4"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kazhuravlev/git-tools/releases/download/v0.6.4/git-tools_Darwin_arm64.tar.gz"
-      sha256 "680973e99c232a5aa45126fae46dccfb9809cde2b7bd6d0b467a410a10284faa"
+      url "https://github.com/kazhuravlev/git-tools/releases/download/v0.7.0/git-tools_Darwin_arm64.tar.gz"
+      sha256 "22cced97271b36281818cb532023e057e9219b97dabb1813ca065dea12b365ee"
 
       def install
         bin.install "gt"
@@ -21,8 +21,8 @@ class GitTools < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kazhuravlev/git-tools/releases/download/v0.6.4/git-tools_Linux_arm64.tar.gz"
-      sha256 "758e7ccf674f8ecb115ab2d106ed426cba1fa1404b342bc21629b5e175b1a52e"
+      url "https://github.com/kazhuravlev/git-tools/releases/download/v0.7.0/git-tools_Linux_arm64.tar.gz"
+      sha256 "2d13eb4a29d0913bc1f17fc8f0cbdb17ca3f4c974a9ccccc189eb44beae9a61d"
 
       def install
         bin.install "gt"
